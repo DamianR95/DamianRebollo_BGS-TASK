4a7ef27feb00b0c108f0054bab11998c36a78bc5
Damian Rebollo <damian_rebollo@hotmail.com>
2024-05-25 18:54:41 -0300

    -Added Camera Follow player along basic actions, movement and Interact.

---
+++ Assets/Scripts/UI/OpenInventory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGS.ProgrammerTask.UI
+{
+    using UnityEngine;
+
+    public class OpenInventory : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _inventoryPanel;
+
+        private void Start()
+        {
+            CloseInventory();
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                ToggleInventory();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape) && _inventoryPanel.activeSelf)
+            {
+                CloseInventory();
+            }
+        }
+
+        void ToggleInventory()
+        {
+            _inventoryPanel.SetActive(!_inventoryPanel.activeSelf);
+            UIHoverPopup.Instance.ClosePopUp();
+        }
+
+        void CloseInventory()
+        {
+            _inventoryPanel.SetActive(false);
+            UIHoverPopup.Instance.ClosePopUp();
+
+        }
+    }
+
+}
