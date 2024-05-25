ec8365c61fde285522d3464085663c1b538c0e66
Damian Rebollo <damian_rebollo@hotmail.com>
2024-05-25 18:58:13 -0300

    -Added Gold gain/spent sfx, and a free BGM.

---
+++ Assets/Scripts/SFX/GoldChange_SFX.cs
@@ -0,0 +1,36 @@
+﻿using BGS.ProgrammerTask.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BGS.ProgrammerTask.SFX
+{
+    public class GoldChange_SFX : MonoBehaviour
+    {
+
+        [SerializeField]
+        private AudioSource sfx;
+
+
+        void Start()
+        {
+            GoldCounter.Instance.OnGoldAdded += RefreshGold;
+            GoldCounter.Instance.OnGoldRemoved += RefreshGold;
+        }
+
+        private void RefreshGold(int obj)
+        {
+            sfx.Play();
+        }
+        private void OnDestroy()
+        {
+            GoldCounter.Instance.OnGoldAdded -= RefreshGold;
+            GoldCounter.Instance.OnGoldRemoved -= RefreshGold;
+        }
+
+    }
+}
+
